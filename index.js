220bb0019f6f3bd450ed1b99c87e4caf9342ad85
POORNESH PRAHALATHAN <83974934+LoheshPrahal@users.noreply.github.com>
2024-12-21 21:49:15 +0530

    static assets in express

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require('path')
 const redditdata = require('./data.json');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
     res.render('home')
 })
