8724786cc872252da11547edf7983656475f8af8
LoheshPrahal <loheshp@outlook.com>
2024-12-24 09:02:50 +0530

    changer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const redditdata = require('./data.json');
+console.log(redditdata);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
